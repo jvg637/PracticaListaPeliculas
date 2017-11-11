c4b2841f98e12f6d8c2c59f25615493d593d5240
Miguel A. Nuñez <miguel.nunez.casado@gmail.com>
2017-11-11 12:55:33 +0100

    Eliminado Pelicula Eliminado BBDD Eliminado BBDDAdapter Actualizado MovieListActivity

--- app/src/main/java/org/upv/practicalistapeliculas/MovieListActivity.java
+++ app/src/main/java/org/upv/practicalistapeliculas/MovieListActivity.java
@@ -1,10 +1,8 @@
 package org.upv.practicalistapeliculas;
 
-import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-import org.upv.practicalistapeliculas.bbdd.BBDDTables;
 
 public class MovieListActivity extends AppCompatActivity {
 
@@ -12,12 +10,6 @@ public class MovieListActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movie);
-
-        /*
-         * Declaramos el controlador de la BBDD y accedemos en modo escritura
-         */
-        /*BBDDTables dbHelper = new BBDDTables(getBaseContext());
-        SQLiteDatabase db = dbHelper.getWritableDatabase();*/
 
 
     }
